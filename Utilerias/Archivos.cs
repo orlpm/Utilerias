1a2bad5389a959f08e9d9bbad963eacd69e6b002
Lobo <wolfkaizen@gmail.com>
2022-04-26 21:13:33 -0500

    Inciando primera versión

---
+++ Utilerias/Archivos.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilerias
+{
+    public class Archivos
+    {
+        public void CreaCSV(string fileName, string nombre, string apellido, int edad)
+        {
+            int miEntero = 45;//En la asignación de un entero solo se ponen números
+            string miString = "Hola Fabi tienes 25 pero te ves de 20";//Para la cadena solo se coloca el texto entre comillas dobles
+
+            if (!File.Exists(fileName))
+            {
+                FileStream fileStream = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+
+                StreamWriter streamWriter = new StreamWriter(fileStream);
+
+                StringBuilder stringBuilder = new StringBuilder();
+
+
+                stringBuilder.Append("Nombre").Append(",").Append("Apellido").Append(",").Append("Edad").Append(",");
+
+                streamWriter.WriteLine(stringBuilder);
+
+                streamWriter.Flush();
+                streamWriter.Close();
+                fileStream.Close();
+            }
+
+            StreamWriter streamWriterRecords = new StreamWriter(fileName, true, Encoding.Latin1);
+            StringBuilder stringBuilderRecord = new StringBuilder();
+
+            stringBuilderRecord.Append(nombre).Append(",").Append(apellido).Append(",").Append(edad).Append(",");
+
+            streamWriterRecords.WriteLine(stringBuilderRecord);
+
+            //streamWriterRecords.WriteLine($"{nombre},{apellido},{edad},");// a mi me late esta
+
+            string linea = nombre + "," + apellido + "," + edad + ",";
+
+            //streamWriterRecords.WriteLine(linea);
+
+            streamWriterRecords.Flush();
+            streamWriterRecords.Close();
+
+        }
+    }
+}
---
+++ Utilerias/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Utilerias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Crear archivo!");
+
+            Archivos archivos = new Archivos();
+
+            for(int i = 0; i<10; i++)
+            {
+                archivos.CreaCSV("D:\\Wolf\\Desktop\\JRH Logistica\\miSegundoCSV.csv", "Fabiola", "Rodriguez", 25);
+            }
+
+            Console.WriteLine("Archivo creado!");
+
+        }
+    }
+}
